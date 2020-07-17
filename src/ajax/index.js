8e054365dceb453d57096dfa779c6ba5defd42fe
Sincebook <40172250+Sincebook@users.noreply.github.com>
2020-07-17 18:57:55 +0800

    first

---
+++ src/ajax/index.js
@@ -0,0 +1,17 @@
+import {
+  get,
+  post,
+  ajax,
+} from './ajax_axios';
+
+export const getIndex = (userName, password) => {
+    return get('http://localhost:8081//page/index/index.html', { userName, password } );
+}
+
+export const doLogin = (userName, password) => {
+  return post('./login', { userName, password } );
+}
+
+export const getUser = (userName, info) => {
+  return get('./user', { userName, info } );
+}
