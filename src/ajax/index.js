152ddb20ea20268992a01498df0b8a302ec18cca
luoxinyuan <1019558287@qq.com>
2020-07-18 15:18:51 +0800

    增加创建活动接口以及奖品类,使接口使用更加方便

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -3,6 +3,7 @@ import {
   post,
   ajax,
 } from './ajax_axios';
+import Award, {addActivity} from "~/ajax/activity";
 
 export const getIndex = (userName, password) => {
     return get('http://localhost:8081//page/index/index.html', { userName, password } );
@@ -15,3 +16,5 @@ export const doLogin = (userName, password) => {
 export const getUser = (userName, info) => {
   return get('./user', { userName, info } );
 }
+
+addActivity('标题','123',[new Award('一等',5),new Award('二等',6)]);
