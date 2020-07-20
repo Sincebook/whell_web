dc7528d1140791a68d9873d1178d7d9deefae384
jayzh <jay__zh@163.com>
2020-07-20 16:58:58 +0800

    activity除了分享 和时间外都已经形成

--- src/page/activity/index.js
+++ src/page/activity/index.js
@@ -1,6 +1,26 @@
-import { displayActivity } from '~/ajax/activity';
-localStorage.setItem('activityId', 1 );
+import { displayActivity, getAmountPeople, finishActivity } from '~/ajax/activity';
+localStorage.setItem('activityId', 8 );
 const id = localStorage.getItem('activityId');
 displayActivity(id).then((data) => {
-    console.log(data)
+    document.getElementById('name').setAttribute('value', data.data.title);
+    document.getElementById('age').setAttribute('value', data.data.details);
+
 })
+getAmountPeople(id).then((data) => {
+    document.getElementById('tel').setAttribute('value', data.data);
+})
+document.getElementById('close').addEventListener('click', endActivity, false);
+
+
+function endActivity() {
+    console.log('进入方法');
+    finishActivity().then((data) => {
+        if (data.data == 2) {
+            window.alert('关闭成功');
+        } else {
+            window.alert('关闭失败');
+        }
+        console.log('触发事件');
+        console.log(data);
+    })    
+}
