6770d80ccc381b77a26189d5020a337238acf95b
Sincebook <40172250+Sincebook@users.noreply.github.com>
2020-08-05 11:39:34 +0800

    修改

--- src/page/add_award/index.js
+++ src/page/add_award/index.js
@@ -1,39 +1,22 @@
 import Award, { addActivity } from '~/ajax/activity';
 import codes from '~/config/codeConfig'
 const createActivityEvent = document.getElementById('sendNow');
-
+import { delegate } from '~/util/elemnet';
+// 初始化奖品及概率
 let awardList = new Array();
 let total_pro = 0;
+let contentList = document.getElementById('contentList');
+// 发布奖品
 createActivityEvent.addEventListener('click', function () {
   let pros = getProbality();
   console.log(pros);
-  // const nameList = document.getElementsByClassName('awardName');
-  // const probabilityList = document.getElementsByClassName('mui-input-numbox');
-  // for (let i in nameList) {
-  //   let name = nameList[i].value;
-  //   let probality = probabilityList[i].value;
-  //   if (name != undefined && probality != undefined) {
-  //     if (name != '' && probality != '') {
-  //       let award = new Award(name, probality);
-  //       awardList[i] = award;
-  //       if (parseInt(probality) >= 0) {
-  //         total_pro += parseInt(probality);
-  //       } else {
-  //         mui.alert('概率输入有误');
-  //         return;
-  //       }
-  //     } else {
-  //       mui.alert('奖品名称不能为空！');
-  //       return;
-  //     }
-  //   }
-  // }
-  // if (total_pro != 100) {
-  //   console.log(total_pro);
-  //   mui.alert('中奖概率之和须为100');
-  //   total_pro = 0;
-  //   return;
-  // }
+  updataPro();
+  if (total_pro != 100) {
+    console.log(total_pro);
+    mui.alert('中奖概率之和须为100');
+    total_pro = 0;
+    return;
+  }
   // console.log(awardList);
   // addActivity(title, details, awardList).then((data) => {
   //   if (data.code == codes.success) {
@@ -44,7 +27,7 @@ createActivityEvent.addEventListener('click', function () {
   //   }
   // });
 });
-
+// 获取当前概率
 function getProbality() {
   const probabilityList = document.getElementsByClassName('mui-input-numbox');
   let probality = [];
@@ -55,17 +38,15 @@ function getProbality() {
         if (parseInt(temple) >= 0 && parseInt(temple) < 99) {
           probality[i] = temple;
         } else {
-          mui.alert('概率输入不能为空');
+          mui.alert('概率在0~99%之间');
           return;
         }
-      } else {
-        mui.alert('概率输入有误');
-      }
+      } 
     }
   }
   return probality;
 }
-
+// 获取当前奖品名称
 function getAwardName() {
   const nameList = document.getElementsByClassName('awardName');
   let names = [];
@@ -80,16 +61,38 @@ function getAwardName() {
     }
   }
 }
-
-function delAward() {
-  
+// 删除奖品
+delegate(contentList, '#delBtn', 'click', delAward, false);
+function delAward(e) {
+  e.delegateTarget.parentNode.parentNode.parentNode.remove();
+  click--;
+  updataPro();
 }
+// 监听输入框
+delegate(contentList, '.mui-input-numbox', 'keydown', inputNum, false);
+function inputNum() {
+  console.log('监听到输入');
+  let id = parseInt(Math.random() * 5000);
+  setTimeout(updataPro(id), 100);
+}
+// 更新当前概率
+const proElment = document.getElementById('probability');
+function updataPro(id) {
+  console.log(id);
+  total_pro = 0;
+  const temple = getProbality();
+  for (let i in temple) {
+    total_pro = total_pro + parseInt(temple[i]);
+  }
+  proElment.innerHTML = 100 - total_pro;
+  console.log('概率更新');
+}
+// 奖品名称，奖品详情初始化
 const title = localStorage.getItem('title');
 const details = localStorage.getItem('details');
-console.log(title);
-console.log(details);
-total_pro = 0;
 
+
+// 添加奖品
 let html = '';
 let add_award = document.getElementById('add_award');
 let click = 0;
@@ -107,13 +110,15 @@ add_award.addEventListener('click', function () {
       <input type="text" placeholder="请输入项目名称" class="awardName" >
     </div>
     <div class="mui-input-row">
-      <label>中奖概率</label>
-      <input class="mui-input-numbox" type="number" maxlength="2"  value="10"/>
-    </div>
+          <label>中奖概率(%)</label>
+          <input class="mui-input-numbox" type="number" maxlength="2" value="10" />
+          <button class="mui-btn-red del_btn" id="delBtn">删除</button>
+        </div>
     </div>
   </div>
 </div>`;
   let node = document.createElement('div');
   node.innerHTML = html;
-  document.getElementById('contentList').appendChild(node);
+  contentList.appendChild(node);
+  updataPro();
 });
